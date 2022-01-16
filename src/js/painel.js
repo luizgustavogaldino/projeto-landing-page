dcadb4e5d08be8b6eadbe815f3e891227a4af049
Luiz Gustavo Galdino da Silva <65501156+luizgustavogaldino@users.noreply.github.com>
2022-01-15 22:02:16 -0300

    commit inicial

---
+++ src/js/painel.js
@@ -0,0 +1,38 @@
+const imagensPainel = document.querySelectorAll('.imagem-painel');
+const setaAvancar = document.getElementById('btn-avancar');
+const setaVoltar = document.getElementById('btn-voltar');
+let imagemAtual = 0;
+
+function esconderImagens () {
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar');
+    })
+}
+
+function mostrarImagens () {
+    imagensPainel[imagemAtual].classList.add('mostrar');
+}
+
+/*Quando clicar na seta pra avançar temos que 
+esconder todas as imagens e mostrar a próxima imagem*/
+
+setaAvancar.addEventListener('click', function () {
+    
+    const totalDeImagens = imagensPainel.length -1;
+    if(imagemAtual === totalDeImagens) {
+        return;            
+    }
+    imagemAtual++;
+    esconderImagens();
+    mostrarImagens();
+})
+
+setaVoltar.addEventListener('click', function() {
+
+    if(imagemAtual === 0) {
+        return;
+    }
+    imagemAtual--;
+    esconderImagens();
+    mostrarImagens();
+})
